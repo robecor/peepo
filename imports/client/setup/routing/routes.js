a0b7124eb136d81a2f24501d172525b06e968633
robecor <robert.corolea@cultofcoders.com>
2019-04-28 15:26:10 +0300

    Register component

--- imports/client/setup/routing/routes.js
+++ imports/client/setup/routing/routes.js
@@ -1,8 +1,13 @@
 import Login from '/imports/client/ui/userPages/Login.jsx';
+import Register from '/imports/client/ui/userPages/Register';
 
 export default [
   {
     path: '/login',
     component: Login
-  }
+  },
+  {
+    path: '/register',
+    component: Register
+  },
 ];
---
+++ imports/client/ui/userPages/Register.jsx
@@ -0,0 +1,29 @@
+import React, {Component} from 'react';
+import {Form, Field} from 'react-final-form';
+
+export default class Register extends Component {
+  onSubmit = (data) => {
+    console.log(data);
+  };
+
+  render() {
+    return (
+      <Form
+        onSubmit={this.onSubmit}
+        render={({handleSubmit, pristine, invalid}) => (
+          <div className="sign-form-cont">
+            <form onSubmit={handleSubmit} className="sign-form">
+              <Field name="userName" component="input" placeholder="Username"/>
+              <Field name="password" component="input" type="password" placeholder="Password"/>
+              <Field name="rePassword" component="input" type="password" placeholder="Re-type password"/>
+
+              <button type="submit" disabled={pristine || invalid}>
+                Register
+              </button>
+            </form>
+          </div>
+        )}
+      />
+    );
+  }
+}
